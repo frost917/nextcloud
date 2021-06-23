7d0db2d199a691d44626b48c61d664e7f05733cb
frost917 <frost97114@gmail.com>
2021-06-24 01:01:17 +0900

    change uid

---
+++ dockerBuild.sh
@@ -0,0 +1,18 @@
+﻿#!/bin/bash 
+gitDir=$(dirname $(realpath $0))
+ver="21"
+
+#  build
+cd  ${gitDir}/${ver}/nginx-alpine/
+docker buildx build \
+    --push \
+    --platform=linux/arm64,linux/amd64 \
+    -t ${REGISTER}/nextcloud:${ver}-nginx-alpine \
+    ./ 
+
+cd ${gitDir}/${ver}/fpm-alpine/
+docker buildx build \
+    --push \
+    --platform=linux/arm64,linux/amd64 \
+    -t ${REGISTER}/nextcloud:${ver}-fpm-alpine \
+    ./
