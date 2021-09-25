81a61cd47db48358f6a9c038d3c61415cf698a4f
JungYeon Kim <frost917@frost917.com>
2021-09-25 14:14:15 +0900

    버전 변경 podman 빌드용 스크립트 추가

--- dockerBuild-22.sh
+++ dockerBuild-22.sh
@@ -1,13 +1,13 @@
 ﻿#!/bin/bash 
 gitDir=$(dirname $(realpath $0))
 ver="22"
-specVer="22.0.0"
+specVer="22.1.1"
 
 #  build
 cd  ${gitDir}/${ver}/nginx-alpine/
 docker buildx build \
     --push \
-    --platform=linux/arm64/v8,linux/amd64,linux/arm/v7,linux/arm/v8 \
+    --platform=linux/arm64/v8,linux/amd64 \
     -t ${REGISTRY}/nextcloud:${ver}-nginx-alpine \
     -t ${REGISTRY}/nextcloud:${specVer}-nginx-alpine \
     ./ 
@@ -15,7 +15,7 @@ docker buildx build \
 cd ${gitDir}/${ver}/fpm-alpine/
 docker buildx build \
     --push \
-    --platform=linux/arm64/v8,linux/amd64,linux/arm/v7,linux/arm/v8 \
+    --platform=linux/arm64/v8,linux/amd64 \
     -t ${REGISTRY}/nextcloud:${ver}-fpm-alpine \
     -t ${REGISTRY}/nextcloud:${specVer}-fpm-alpine \
     ./
---
+++ podmanBuild.sh
@@ -0,0 +1,32 @@
+﻿#!/bin/sh
+
+gitDir=$(dirname $(realpath $0))
+ver="22"
+specVer="22.1.1"
+serviceName="nextcloud"
+
+cd  ${gitDir}/${ver}/nginx-alpine/
+podman build \
+    --pull \
+    --no-cache \
+    --platform=linux/arm64/v8,linux/amd64 \
+    -t ${REGISTRY}/$serviceName:${ver}-nginx-alpine \
+    -t ${REGISTRY}/$serviceName:${specVer}-nginx-alpine \
+    ./
+
+podman image push \
+    ${REGISTRY}/$serviceName:${ver}-nginx-alpine \
+    ${REGISTRY}/$serviceName:${specVer}-nginx-alpine
+
+cd ${gitDir}/${ver}/fpm-alpine/
+podman build \
+    --pull \
+    --no-cache \
+    --platform=linux/arm64/v8,linux/amd64 \
+    -t ${REGISTRY}/$serviceName:${ver}-fpm-alpine \
+    -t ${REGISTRY}/$serviceName:${specVer}-fpm-alpine \
+    ./
+
+podman image push \
+    ${REGISTRY}/$serviceName:${ver}-fpm-alpine \
+    ${REGISTRY}/$serviceName:${specVer}-fpm-alpine
